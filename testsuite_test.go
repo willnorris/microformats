a8c6275f92e0cc4c8cc01fe3dec3ea06504dbea0
Will Norris <will@willnorris.com>
2016-06-22 07:40:21 -0700

    run testsuite tests in parallel

--- testsuite_test.go
+++ testsuite_test.go
@@ -63,6 +63,7 @@ var skipTests = []string{
 func TestSuite(t *testing.T) {
 	for _, version := range []string{"microformats-v2"} {
 		t.Run(version, func(t *testing.T) {
+			t.Parallel()
 			base := filepath.Join("testdata", "tests", version)
 			tests, err := listTests(base)
 			if err != nil {
@@ -71,6 +72,7 @@ func TestSuite(t *testing.T) {
 
 			for _, test := range tests {
 				t.Run(test, func(t *testing.T) {
+					t.Parallel()
 					for _, skip := range skipTests {
 						if path.Join(version, test) == skip {
 							t.Skip()
