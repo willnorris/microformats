7685e147749dac8579b749bc9bad3e4b287550bd
Shane Becker <veganstraightedge@gmail.com>
2017-05-26 20:08:23 -0700

    Add bootstrap/responsive meta tags

--- cmd/gomfweb/main.go
+++ cmd/gomfweb/main.go
@@ -90,6 +90,9 @@ func index(w http.ResponseWriter, r *http.Request) {
 var tpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
   <title>Go Microformats Parser</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   <style>
