62757f77a368a29cf8596afd0694f9f86749f31e
Peter Stuifzand <peter.stuifzand@gmail.com>
2018-08-15 22:22:32 +0200

    Stop skipping all tests that now work

--- testsuite_test.go
+++ testsuite_test.go
@@ -39,12 +39,9 @@ import (
 
 // skip the tests which we don't pass yet
 var skipTests = []string{
-	"microformats-v2/h-entry/summarycontent",
 	"microformats-v2/h-entry/urlincontent",
 	"microformats-v2/h-event/concatenate",
 	"microformats-v2/h-event/dates",
-	"microformats-v2/h-feed/implied-title",
-	"microformats-v2/h-recipe/all",
 	"microformats-v2/h-review/vcard",
 	"microformats-v2/rel/duplicate-rels",
 	"microformats-v2/rel/varying-text-duplicate-rels",
