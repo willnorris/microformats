075e39c537cf7332385c09d6d288f48814dd4cd6
Will Norris <will@willnorris.com>
2018-08-31 19:03:56 +0000

    gomf: add selector support

--- cmd/gomf/main.go
+++ cmd/gomf/main.go
@@ -1,27 +1,40 @@
 // The gomf tool is a command line tool which parses microformats from the
 // specified URL.
+//
+// Usage: gomf <URL> [optional selector]
 package main
 
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
 
+	"github.com/PuerkitoBio/goquery"
 	"willnorris.com/go/microformats"
 )
 
 func main() {
-	resp, err := http.Get(os.Args[1])
+	u, _ := url.Parse(os.Args[1])
+	resp, err := http.Get(u.String())
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	urlparsed, _ := url.Parse(os.Args[1])
-	data := microformats.Parse(resp.Body, urlparsed)
+	defer resp.Body.Close()
+
+	var data *microformats.Data
+	if len(os.Args) > 2 {
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = microformats.ParseNode(doc.Find(os.Args[2]).Get(0), u)
+	} else {
+		data = microformats.Parse(resp.Body, u)
+	}
 
 	json, _ := json.MarshalIndent(data, "", "  ")
-
 	fmt.Println(string(json))
 }
